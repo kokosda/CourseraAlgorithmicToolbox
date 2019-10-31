66c1c46f906f88f3eaa1a855b95a2704babf1cdc
Victor Ponamarev <vponamrev@gmail.com>
2019-10-31 10:51:04 +0300

    Fibonaccu huge - doing modulo instead of calculating period of Fibo.

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
@@ -16,49 +16,23 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
     return current % m;
 }
 
-std::string calculate_period(long long n, long long m) {
-    if (n <= 1)
-        return "1";
-
+long long calculate_period(long long n, long long m) {
     long long previous = 0;
     long long current  = 1;
-    long long remainder;
-    const std::string periodIndicator = "01";
-    int periodLength = periodIndicator.length();
-    std::string remainderStr;
-    std::string tail = periodIndicator;
-    std::string result(periodIndicator);
     long long length;
 
-    for (long long i = 2; i < n - 1; ++i) {
+    for (long long i = 0; i < m * m; ++i) {
         long long tmp_previous = previous;
         previous = current;
-        current = tmp_previous + current;
-        remainder = current % m;
-
-        remainderStr = std::to_string(remainder);
-
-        std::cout << "tail before: " << tail << std::endl;
-
-        tail = tail.substr(1) + remainderStr;
-
-        std::cout << "tail after: " << tail << std::endl;
-
-        if (tail.compare(tail.length() - periodLength, periodLength, periodIndicator) != 0) {
-            result += remainderStr;
-            std::cout << "result0: " << result << " tail0: " << tail << std::endl;
-            continue;
+        current = (tmp_previous + current) % m;
+        
+        if (previous == 0 && current == 1) {
+            length = i + 1;
+            break;
         }
-
-        length = i - 1;
-        std::cout << "length: " << length << std::endl;
-        break;
     }
 
-    result = result.substr(0, length);
-    std::cout << "result: " << result << std::endl;
-
-    return result;
+    return length;
 }
 
 int get_fibonacci_number(int n) {
@@ -78,16 +52,9 @@ int get_fibonacci_number(int n) {
 }
 
 long long get_fibonacci_huge_fast(long long n, long long m) {
-    long long fibonnaci_number;
-
-    if (n <= 10) {
-        fibonnaci_number = get_fibonacci_number(n);
-    }
-    else {
-        std::string period = calculate_period(n, m);
-        long long remainder = n % period.length();
-        fibonnaci_number = get_fibonacci_number(remainder);
-    }
+    long long period = calculate_period(n, m);
+    long long remainder = n % period;
+    int fibonnaci_number = get_fibonacci_number(remainder);
     
     long long result = fibonnaci_number % m;
     return result;
