ddf59a6b4890834ed212a47dd5ec74115bd3045d
Victor Ponamarev <vponamrev@gmail.cm>
2019-12-05 12:21:12 +0300

    Money change - fixes.

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -3,17 +3,15 @@
 
 using std::vector;
 
-const static vector<int> COINS { 1, 3, 4 };
-const static int END = 1000000;
-
 int get_change(int money) {
+  const vector<int> COINS { 1, 3, 4 };
+  const int END = 1000000; 
   vector<int> min_num_coins(money);
-  // min_num_coins.push_back(0);
+  min_num_coins[0] = 0;
   int num_coins = 0;
   size_t coins_size = COINS.size();
 
-  for (int m = 1; m <= money; m++) {
-    // min_num_coins.push_back(END);
+  for (int m = 1; m < money; m++) {
     min_num_coins[m] = END;
 
     for (int i = 0; i < coins_size; i++) {
@@ -26,13 +24,15 @@ int get_change(int money) {
       }
     }
   }
-  return min_num_coins[money];
+
+  int result = min_num_coins[money - 1];
+  return result;
 }
 
 int main() {
   int m;
-  // std::cin >> m;
-  m = 34;
+  std::cin >> m;
+  // m = 34;
   std::cout << get_change(m) << '\n';
   system("pause");
   return 0;
