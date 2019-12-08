85d1f5ff43d0873072b94260ffa3b8a5ccb784fa
Victor Ponamarev <vponamrev@gmail.cm>
2019-12-08 20:50:07 +0300

    Money change - fix of algorithm.

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -6,12 +6,12 @@ using std::vector;
 int get_change(int money) {
   const vector<int> COINS { 1, 3, 4 };
   const int END = 1000000; 
-  vector<int> min_num_coins(money);
+  vector<int> min_num_coins(money + 1);
   min_num_coins[0] = 0;
   int num_coins = 0;
   size_t coins_size = COINS.size();
 
-  for (int m = 1; m < money; m++) {
+  for (int m = 1; m <= money; m++) {
     min_num_coins[m] = END;
 
     for (int i = 0; i < coins_size; i++) {
@@ -22,10 +22,13 @@ int get_change(int money) {
           min_num_coins[m] = num_coins;
         }
       }
+      else {
+        break;
+      }
     }
   }
 
-  int result = min_num_coins[money - 1];
+  int result = min_num_coins[money];
   return result;
 }
 
