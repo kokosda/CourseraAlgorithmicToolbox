6aae020506cb108b5e7ed1bd47f7d1d8bf23a545
Victor Ponamarev <vponamrev@gmail.cm>
2019-12-10 11:27:34 +0300

    Primitive calculator - dynamic programming final implementation.

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -4,6 +4,19 @@
 
 using std::vector;
 
+void show_vector(const vector<int> &a, std::string name) {
+  bool enabled = true;
+
+  if (!enabled)
+    return;
+
+  std::cout << name << " size " << a.size() << ": ";
+  for (int i = 0; i < a.size(); i++) {
+    std::cout << a[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
 vector<int> optimal_sequence(int n) {
   std::vector<int> sequence;
   while (n >= 1) {
@@ -28,48 +41,57 @@ vector<int> optimal_sequence(int n) {
   return sequence;
 }
 
-const vector<int> OPERATIONS = { 1, 2, 3};
-
-int get_operation_result(int m, int operation_index) {
-  if (operation_index == 0)
-    return m + OPERATIONS[operation_index];
-  else
-    return m * OPERATIONS[operation_index];
-}
-
 vector<int> optimal_sequence_dynamic(int n) {
-  const int END = -1; 
-  vector<int> sequence(n + 1);
-  vector<int> result_sequence;
-  sequence[0] = 1;
-  int num_operations = 0;
-  size_t operations_size = OPERATIONS.size();
+    vector<int> arr(n + 1);
+    arr[0] = 0;
+    size_t arr_size = arr.size();
+    vector<int> sequence;
 
-  for (int m = 1; m <= n; m++) {
-    sequence[m] = END;
+    for (int i = 1; i < arr_size; i++) {
+        arr[i] = arr[i - 1] + 1;
 
-    for (int i = 0; i < operations_size; i++) {
-      int operation_result = get_operation_result(m, i);
+        if (i % 2 == 0) {
+          arr[i] = std::min(arr[i / 2] + 1, arr[i]);
+        }
 
-      if (operation_result)
-      sequence[m] = std::max(sequence[m], operation_result);
+        if (i % 3 == 0) {
+          arr[i] = std::min(arr[i / 3] + 1, arr[i]); 
+        }
     }
-  }
 
-  return result_sequence;
+    // show_vector(arr, "arr");
+
+    int i = n;
+    while (i > 1) {
+        sequence.push_back(i);
+
+        if (arr[i - 1] == arr[i] - 1) {
+            i = i - 1;
+        }
+        else if (i % 2 == 0 && (arr[i / 2] == arr[i] - 1)) {
+            i = i / 2; 
+        }
+        else if (i % 3 == 0 && (arr[i / 3] == arr[i] - 1)) {
+            i = i / 3;
+        }
+    }
+
+    sequence.push_back(1);
+    reverse(sequence.begin(), sequence.end());
+    return sequence;
 }
 
 // 1 3 9 11
 
 int main() {
   int n;
-  // std::cin >> n;
-  n = 10;
+  std::cin >> n;
+  // n = 96234;
   vector<int> sequence = optimal_sequence_dynamic(n);
   std::cout << sequence.size() - 1 << std::endl;
   for (size_t i = 0; i < sequence.size(); ++i) {
     std::cout << sequence[i] << " ";
   }
-  std::cout << std::endl;
-  system("pause");
+  // std::cout << std::endl;
+  // system("pause");
 }
