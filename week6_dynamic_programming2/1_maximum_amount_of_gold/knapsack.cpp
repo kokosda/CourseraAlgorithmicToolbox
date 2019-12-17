ec01c1e0469936f5684e498a1300750818ce861a
Victor Ponamarev <vponamrev@gmail.cm>
2019-12-17 11:34:10 +0300

    Knapsack - 1st try.

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -3,23 +3,51 @@
 
 using std::vector;
 
+const int ROW_MAX = 1000 + 1;
+const int COL_MAX = 300 + 1;
+
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+  int value[ROW_MAX][COL_MAX];
+  size_t n = w.size();
+
+  for (size_t j = 0; j <= n; j++) {
+    value[0][j] = 0;
+  }
+
+  for (int w = 0; w <= W; w++) {
+    value[w][0] = 0;
+  }
+
+  int val, wi, vi;
+
+  for (size_t i = 1; i <= n; i++) {
+    for (int w = 1; w <= W; i++) {
+      value[w][i] = wi = value[w][i - 1];
+
+      if (wi <= w) {
+        val = value[w - wi][i - 1];
+      }
     }
   }
-  return current_weight;
+
+  int result = 0;
+  return result;
+}
+
+void insert_data(int &W, vector<int> &w) {
+  W = 10;
+  w = { 1, 4, 8 };
 }
 
 int main() {
   int n, W;
-  std::cin >> W >> n;
+  // std::cin >> W >> n;
   vector<int> w(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
-  }
+  // for (int i = 0; i < n; i++) {
+  //   std::cin >> w[i];
+  // }
+  insert_data(W, w);
   std::cout << optimal_weight(W, w) << '\n';
+  system("pause");
+  return 0;
 }
