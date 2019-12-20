cd28ba8e27d3ef6a1c08511db8321bf38a6b89c1
Victor Ponamarev <vponamrev@gmail.cm>
2019-12-20 09:48:38 +0300

    Dynamic programming - trying to implement knapsack withour repition problem.

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,36 +1,92 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
-const int ROW_MAX = 1000 + 1;
-const int COL_MAX = 300 + 1;
+const int WEIGHT_MAX = 1000 + 1;
+const int ITEMS_COUNT_MAX = 300 + 1;
+
+void show_results(int distance[][WEIGHT_MAX], int n, int m) {
+  bool enabled = true;
+
+  if (!enabled) {
+    return;
+  }
+
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= m; j++) {
+      std::cout << distance[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
 
 int optimal_weight(int W, const vector<int> &w) {
-  int value[ROW_MAX][COL_MAX];
-  size_t n = w.size();
+  int weights[ITEMS_COUNT_MAX][WEIGHT_MAX];
+  const int w_size = w.size();
 
-  for (size_t j = 0; j <= n; j++) {
-    value[0][j] = 0;
+  for (int i = 0; i <= w_size; i++) {
+    weights[i][0] = 0;
   }
 
-  for (int w = 0; w <= W; w++) {
-    value[w][0] = 0;
+  for (int i = 0; i <= W; i++) {
+    weights[0][i] = 0;
   }
 
-  int val, wi, vi;
+  int prev, cur;
 
-  for (size_t i = 1; i <= n; i++) {
-    for (int w = 1; w <= W; i++) {
-      value[w][i] = wi = value[w][i - 1];
-
-      if (wi <= w) {
-        val = value[w - wi][i - 1];
+  for (int i = 1; i <= w_size; i++) {
+    for (int j = 1; j <= W; j++) {
+      if (j >= w[i]) {
+        weights[i][j] = w[i] + weights[i - 1][j - w[i]];
+        show_results(weights, w_size, W);
+      }
+      else {
+        weights[i][j] = weights[i - 1][j];
+        show_results(weights, w_size, W);
       }
     }
   }
 
-  int result = 0;
+  int result = weights[w_size][W];
+  return result;
+}
+
+int optimal_weight_2(int W, const vector<int> &w) {
+  int weights[ITEMS_COUNT_MAX][WEIGHT_MAX];
+  const int w_size = w.size();
+
+  for (int i = 0; i <= w_size; i++) {
+    weights[i][0] = 0;
+  }
+
+  for (int i = 0; i <= W; i++) {
+    weights[0][i] = 0;
+  }
+
+  int value, val;
+
+  for (int i = 1; i <= w_size; i++) {
+    for (int j = 1; j <= W; j++) {
+      value = weights[i - 1][j];
+
+      if (w[i] <= j) {
+        val = (weights[i - 1][j - w[i]]) + w[i];
+
+        if (value < val) {
+          value = val;
+        }
+
+        weights[i][j] = value;
+      }
+
+      show_results(weights, w_size, W);
+    }
+  }
+
+  int result = weights[w_size][W];
   return result;
 }
 
@@ -47,7 +103,7 @@ int main() {
   //   std::cin >> w[i];
   // }
   insert_data(W, w);
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << optimal_weight_2(W, w) << '\n';
   system("pause");
   return 0;
 }
