6c9866e6d07fc4424017a1796ec011ec0dd703df
Victor Ponamarev <vponamrev@gmail.cm>
2019-12-24 10:41:01 +0300

    Dynamic programming - partitioning sum of subsets of size K.

--- week6_dynamic_programming2/2_partitioning_souvenirs/partition3.cpp
+++ week6_dynamic_programming2/2_partitioning_souvenirs/partition3.cpp
@@ -4,8 +4,8 @@
 
 using std::vector;
 
-const int VALUE_MAX = 300 + 1;
-const int ITEMS_COUNT_MAX = 200 + 1;
+const int VALUE_MAX = 30 + 1;
+const int ITEMS_COUNT_MAX = 20 + 1;
 
 void show_vector(const vector<int> &a, std::string name) {
   bool enabled = true;
@@ -20,7 +20,35 @@ void show_vector(const vector<int> &a, std::string name) {
   std::cout << std::endl;
 }
 
-void show_2d_array(int weights[][VALUE_MAX], int n, int m) {
+void show_1d_array(bool* array, int n, std::string name) {
+  bool enabled = false;
+
+  if (!enabled) {
+    return;
+  }
+  
+  std::cout << name << " size " << n << ": ";
+  for (int i = 0; i < n; i++) {
+      std::cout << array[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+void show_1d_array(int* array, int n, std::string name) {
+  bool enabled = false;
+
+  if (!enabled) {
+    return;
+  }
+  
+  std::cout << name << " size " << n << ": ";
+  for (int i = 0; i < n; i++) {
+      std::cout << array[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+void show_2d_array(int array[][VALUE_MAX], int n, int m) {
   bool enabled = false;
 
   if (!enabled) {
@@ -29,100 +57,102 @@ void show_2d_array(int weights[][VALUE_MAX], int n, int m) {
 
   for (int i = 0; i <= n; i++) {
     for (int j = 0; j <= m; j++) {
-      std::cout << weights[i][j] << " ";
+      std::cout << array[i][j] << " ";
     }
     std::cout << std::endl;
   }
   std::cout << std::endl;
 }
 
-vector<int> get_optimal_weight(int W, const vector<int> &w, int &optimal_weight) {
-  int weights[ITEMS_COUNT_MAX][VALUE_MAX];
-  const int w_size = w.size();
+void show_2d_array(bool** array, int n, int m) {
+  bool enabled = true;
 
-  for (int i = 0; i <= w_size; i++) {
-    for (int j = 0; j <= W; j++) {
-        if (i==0 || j==0)
-          weights[i][j] = 0;
-        else if (w[i - 1] <= j)
-          weights[i][j] = std::max(w[i - 1] + weights[i - 1][j - w[i - 1]],  weights[i - 1][j]);
-        else
-          weights[i][j] = weights[i - 1][j];
-    }
+  if (!enabled) {
+    return;
   }
 
-  optimal_weight = weights[w_size][W];
-  int temp_optimal_weight = optimal_weight;
-  int temp_w = W;
-  vector<int> result;
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= m; j++) {
+      std::cout << array[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
 
-  for (int i = w_size; i > 0 && temp_optimal_weight > 0; i--) {
-    if (temp_optimal_weight == weights[i - 1][temp_w]) {
-      continue;
-    }
-    else {
-        result.push_back(w[i - 1]);
-        temp_optimal_weight = temp_optimal_weight - w[i - 1]; 
-        temp_w = temp_w - w[i - 1]; 
-    }
+bool partition3 (vector<int> &A) {
+  const int k = 3;
+  int N = A.size();
+
+  std::sort(A.begin(), A.end());
+
+  int sum = 0;
+
+  for (int i = 0; i < N; i++) {
+    sum += A[i];
   }
 
-  show_vector(result, "backtrace");
+  int target = sum / k;
 
+  if (sum % k > 0 || A[N - 1] > target)
+    return false;
+
+  int size = 1 << N;
+  bool* dp = new bool[size];
+  dp[0] = true;
+
+  for (int i = 1; i < size; i++) {
+    dp[i] = false;
+  }
+
+  int* total = new int[size];
+
+  for (int i = 0; i < size; i++) {
+    total[i] = 0;
+  }
+
+  for (int state = 0; state < size; state++) {
+      if (!dp[state]) {
+        continue;
+      }
+        
+      for (int i = 0; i < N; i++) {
+          int future = state | (1 << i);
+
+          if (state != future && !dp[future]) {
+              if (A[i] <= target - (total[state] % target)) {
+                  dp[future] = true;
+                  total[future] = total[state] + A[i];
+                  show_1d_array(dp, size, "dp");
+                  show_1d_array(total, size, "tl");
+              } else {
+                  break;
+              }
+          }
+      }
+  }
+  
+  bool result = dp[size - 1];
+  delete[] dp, total;
   return result;
 }
 
-int partition3(vector<int> &A) {
-  int total = 0;
-  int A_size = A.size();
-
-  for (int i = 0; i < A_size; i++) {
-    total += A[i];
-  }
-
-  if (total % 3) {
-    return 0;
-  }
-
-  int W = total / 3;
-  int i = 3;
-  vector<int> copy_A(A);
-
-  show_vector(copy_A, "copy_A");
-
-  for (int i = 3; i > 0; i--) {
-    int optimal_weight = 0;
-    vector<int> backtrace = get_optimal_weight(W, copy_A, optimal_weight);
-
-    if (optimal_weight != W) {
-      return 0;
-    }
-
-    for (int i = 0; i < backtrace.size(); i++) {
-      std::vector<int>::iterator position = std::find(copy_A.begin(), copy_A.end(), backtrace[i]);
-      if (position != copy_A.end()) {
-          copy_A.erase(position);
-      }
-    }
-    show_vector(copy_A, "copy_A");
-  }
-
-  return 1;
-}
-
 void insert_data(vector<int> &A) {
-  A = { 17, 59, 34, 57, 17, 23, 67, 1, 18, 2, 59 };
+  // A = { 17, 59, 34, 57, 17, 23, 67, 1, 18, 2, 59 };
+  A = { 3, 3, 3 };
   // 34 + 67 + 17 = 23 + 59 + 1 + 17 + 18 = 59 + 2 + 57.
+  // A = { 23, 23, 23, 11, 11, 11, 4, 4, 4, 7, 7, 7, 17, 17, 17 };
+  // A = { 23, 23, 11, 11, 4, 4, 7, 7, 17, 17 };
 }
 
 int main() {
   int n = 0;
-  // std::cin >> n;
+  std::cin >> n;
   vector<int> A(n);
-  insert_data(A);
-  // for (size_t i = 0; i < A.size(); ++i) {
-  //   std::cin >> A[i];
-  // }
+  // insert_data(A);
+  for (size_t i = 0; i < A.size(); ++i) {
+    std::cin >> A[i];
+  }
   std::cout << partition3(A) << '\n';
   system("pause");
   return 0;
